6af1e8272c4c9c6fe05cdb76fa4904c23fd420d4
Miracle3456 <miraclepayton123@gmail.com>
2025-07-10 15:35:49 +0300

    registered models in admn.py file

--- ple_app/admin.py
+++ ple_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Student_Model , GradesModel
 # Register your models here.
+admin.site.register(Student_Model)
+admin.site.register(GradesModel)
